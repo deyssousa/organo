c752520d95041efe0b995a74e2b46e13920321ec
Deys Sousa <deyslima38@gmail.com>
2023-10-21 15:38:59 -0300

    organo

---
+++ src/Componetes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    //JSX
+    return (
+        <header className="banner">
+        <img src="/Imagens/banner.png" alt="banner principal da pagina do organo"/>
+        </header>
+    )
+}
+
+export default Banner
